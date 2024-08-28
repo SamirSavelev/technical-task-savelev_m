314da09d301e76cf18e983e5e08d54e9b145d2d5
Michael Savelev <michaeloriginal@yandex.ru>
2024-08-28 22:11:03 +0300

    Predeploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/technical-task-savelev_m/',
   resolve: {
     alias: {
       app: path.resolve(__dirname, './src/app'),
