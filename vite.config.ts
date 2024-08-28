dcd0129c7ef38ca050f6f1c2d4f2aead359df114
Michael Savelev <michaeloriginal@yandex.ru>
2024-08-28 22:13:27 +0300

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   base: '/technical-task-savelev_m/',
+  build: {
+    outDir: 'dist',
+  },
   resolve: {
     alias: {
       app: path.resolve(__dirname, './src/app'),
