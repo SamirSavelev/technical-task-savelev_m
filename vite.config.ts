90010d896f1fec0093cc6d5531974d6246f67d43
Michael Savelev <michaeloriginal@yandex.ru>
2024-08-28 19:14:40 +0300

    Launch mock api

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '/': path.resolve(__dirname, './src'),
+      data: path.resolve(__dirname, './src/data'),
+      api: path.resolve(__dirname, './src/api'),
+      assets: path.resolve(__dirname, './src/assets'),
+      types: path.resolve(__dirname, './src/types'),
+      lib: path.resolve(__dirname, './src/lib'),
+    },
+  },
+});
